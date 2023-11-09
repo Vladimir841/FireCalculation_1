3d58c5683ba3bc6405c9ce3bc0c1d1deb0a7ac94
Vladimir841 <Sam9144@yandex.ru>
2023-11-09 12:01:56 +0300

    Commit 3

--- src/FireCalculation.java
+++ src/FireCalculation.java
@@ -1,73 +1,72 @@
 import java.util.*;
 
 class FireCalculation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OutOfRangeException {
 
-        double capital, outProc, cost, growthPercentage, changeIMB, inflation;
-        int startYear, indexForCalc, hundred, minYear, maxYear;
-
-        hundred = 100;          // 100%
-        outProc = 0.0;          // Начальное значение % изъятия
-        growthPercentage = 0.5; // Шаг увеличения процента изъятия
-
-        minYear = 2002;         // Минимальное допустимое значение для ввода
-        maxYear = 2021;         // Максимальное допустимое значение для ввода
+        final int MINYEAR = 2002;            // Минимальное допустимое значение для ввода
+        final int MAXYEAR = 2021;            // Максимальное допустимое значение для ввода
 
         Scanner sc = new Scanner(System.in);
-        startYear = sc.nextInt();
+        final int STARTYEAR = sc.nextInt();  // Считываем данные от пользователя
 
         try {
             // Если год в диапозоне 2002 - 2021 включительно тогда
-            if (startYear >= minYear && startYear <= maxYear) {
+            if (STARTYEAR >= MINYEAR && STARTYEAR <= MAXYEAR) {
 
-                indexForCalc = startYear - minYear;  // Индекс для обращения к данным в классе Constants
-                capital = capitalInit(indexForCalc); // Инициализация переменной capital
-                cost = capital * outProc / hundred;  // Сколько выводим на расходы (цифрой)
+                int indexForCalc;
 
-                for (int count = startYear; count <= maxYear; ++count) {
+                // 100.0%
+                final double HUNDRED = 100.0;
+
+                // Шаг прироста процента изъятия
+                final double GROWTHPERCENTAGE = 0.5;
+
+                double cost;
+                double capital;
+
+                for (double outProc = 0.0; outProc <= HUNDRED; outProc += GROWTHPERCENTAGE) {
 
                     //Если год начала жизни на проценты 2021 снимаем все и доживаем до 2022 года
-                    if (startYear == maxYear) {
-                        System.out.println(hundred);
+                    if (STARTYEAR == MAXYEAR) {
+                        System.out.println(HUNDRED);
                         break;
                     }
-                    // В начале года вычитаем расходы
-                    capital = capitalMinusCost(capital, cost);
 
-                    // Получаем данные об инфляции
-                    inflation = inflationCurrentYear(indexForCalc);
+                    indexForCalc = STARTYEAR - MINYEAR;     // Индекс для обращения к данным в классе Constants
+                    capital = capitalInit(indexForCalc); // Инициализация переменной capital
+                    cost = capital * outProc / HUNDRED;  // Сколько изымается ( числом )
 
-                    // Увеличение расходов в результате инфляции
-                    cost += recalculcWithdrawalAmount(cost, inflation, hundred);
+                    double inflation;
+                    double changeIMB;
 
-                    // Как и на сколько в % изменился ИМБ за год
-                    changeIMB = IndexChange(indexForCalc);
+                    for (int count = STARTYEAR; count < MAXYEAR; ++count) {
 
-                    // Изменение капитала в зависимости от изменения ИМБ
-                    capital += capital * changeIMB / hundred;
+                        // В начале года вычитаем расходы
+                        capital = capitalMinusCost(capital, cost);
 
-                    indexForCalc++; // Увеличиваен иднекс для обращения в класс Coststant
+                        // Получаем данные об инфляции
+                        inflation = inflationCurrentYear(indexForCalc);
 
-                    /*
+                        // Увеличение расходов в результате инфляции
+                        cost += recalculcWithdrawalAmount(cost, inflation, HUNDRED);
 
-                       Если остается слишком много денег в наследство, сбрасываем переменные на
-                       начальные значения, увеличиваем процент изъятия на 0.5% и пересчитываем.
+                        // Как и на сколько в % изменился ИМБ за год
+                        changeIMB = IndexChange(indexForCalc);
 
-                    */
+                        // Изменение капитала в зависимости от изменения ИМБ
+                        capital += capital * changeIMB / HUNDRED;
 
-                    if (capital > 0 && count == maxYear) {
-                        indexForCalc = startYear - minYear;
-                        capital = Constants.MOEX_RATE[indexForCalc];
-                        outProc += growthPercentage;        //Сколько изымается ( в процентах )
-                        cost = capital * outProc / hundred; //Сколько изымается ( числом )
-                        capital -= cost;
-                        count = startYear;
+                        // Увеличиваен иднекс для обращения в класс Coststants
+                        indexForCalc++;
+
+                        // Если тут ИСТИНА, значит при таком проценте изъятия не доживем до 2022 года
+                        if (capital < cost) {
+                            outProc -= GROWTHPERCENTAGE; // Процент изъятия уменьшается на 0.5 ( шаг )
+                            System.out.println(outProc);
+                            break;
+                        }
                     }
-
-                    // Если тут ИСТИНА, значит при таком проценте изъятия не доживем до 2022 года
                     if (capital < cost) {
-                        outProc -= growthPercentage; // Процент изъятия уменьшается на 0.5 ( шаг )
-                        System.out.println(outProc);
                         break;
                     }
                 }
@@ -88,16 +87,17 @@ class FireCalculation {
     }
 
     private static double IndexChange(int imbYear) {
-        return (Constants.MOEX_RATE[imbYear + 1] - Constants.MOEX_RATE[imbYear]) * 100 /
+        final double HUNDRED_1 = 100;
+        return (Constants.MOEX_RATE[imbYear + 1] - Constants.MOEX_RATE[imbYear]) * HUNDRED_1 /
                 Constants.MOEX_RATE[imbYear];
     }
 
-    private static double recalculcWithdrawalAmount(double withdrawalAmount, double inflation, int hundred) {
+    private static double recalculcWithdrawalAmount(double withdrawalAmount, double inflation, double hundred) {
         return inflation / hundred * withdrawalAmount;
     }
 
     private static double inflationCurrentYear(int indexForCalc) {
-        return Constants.INFLATION_RATE[indexForCalc + 1];
+        return Constants.INFLATION_RATE[indexForCalc];
     }
 
 }
