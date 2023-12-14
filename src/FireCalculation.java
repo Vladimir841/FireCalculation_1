4c6b2def7dddf7cfdaa68710eb2aa22616c089b5
Vladimir841 <Sam9144@yandex.ru>
2023-12-14 09:47:41 +0300

    - Убрал вложенный for в класс Work метод rasCapital

--- src/FireCalculation.java
+++ src/FireCalculation.java
@@ -1,43 +1,16 @@
-class FireCalculation {
+public class FireCalculation {
     public static void main(String[] args) {
 
-        final double GROWTHPERCENTAGE = 0.5;
+        double answer = 0.0;
 
-        finish:
+        for(double outProc = Work.GROW; outProc <= Work.HUNDRED; outProc += Work.GROW) {
 
-        for (double outProc = 0.0; outProc <= Work.HUNDRED; outProc += GROWTHPERCENTAGE) {
+            answer = Work.rasCapital(outProc);
 
-            int indexForCalc;
-            double cost;
-            double capital;
-
-            if (Work.STARTYEAR == Work.MAXYEAR) {
-                System.out.println(Work.HUNDRED);
+            if (Work.out) {
                 break;
             }
-
-            indexForCalc = Work.STARTYEAR - Work.MINYEAR;
-            capital = Work.capitalInit(indexForCalc);
-            cost = Work.Rc(capital, outProc);
-
-            for (int count = Work.STARTYEAR; count < Work.MAXYEAR; ++count) {
-
-                double inflation;
-                double changeIMB;
-
-                capital = Work.capitalMinusCost(capital, cost);
-                inflation = Work.inflationCurrentYear(indexForCalc);
-                cost += Work.recalculcWithdrawalAmount(cost, inflation);
-                changeIMB = Work.IndexChange(indexForCalc);
-                capital += Work.CapitalF(capital, changeIMB);
-
-                indexForCalc++;
-
-                if (capital < cost) {
-                    Work.Pr(outProc, GROWTHPERCENTAGE);
-                    break finish;
-                }
-            }
         }
+        System.out.println(answer);
     }
 }
--- src/Work.java
+++ src/Work.java
@@ -1,57 +1,72 @@
 import java.util.Scanner;
-
 public class Work {
-
     final static int MINYEAR = 2002;
-    final static int MAXYEAR = 2021;
-    final static int STARTYEAR = EnterYear();
+    final static int MAXYEAR = 2022;
+    final static int STARTYEAR = enterYear();
     final static double HUNDRED = 100.0;
+    final static double GROW = 0.5;
+    public static boolean out = false;
 
-
-    public static double capitalMinusCost(double capital, double withdrawalAmount) {
-        return capital - withdrawalAmount;
+    public static int enterYear() {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x == 2022) {
+            return x + 1;
+        }
+        return x;
     }
 
+    public static double rasCapital(double outProc) {
 
-    public static double CapitalF (double capital, double changeIMB) {
-        return capital * changeIMB / 100;
+        int indexForCalc = STARTYEAR - MINYEAR;
+        double capital = Constants.MOEX_RATE[indexForCalc];
+        double cost = rc(capital, outProc);
+
+        for (int count = Work.STARTYEAR; count < Work.MAXYEAR; ++count) {
+
+            double inflation;
+            double changeIMB;
+
+            if (capital < cost) {
+                outProc -= GROW;
+                out = true;
+                break;
+            }
+
+            capital = Work.capitalMinusCost(capital, cost);
+            inflation = Work.inflationCurrentYear(indexForCalc);
+            cost += Work.recalculcWithdrawalAmount(cost, inflation);
+            changeIMB = Work.IndexChange(indexForCalc);
+            capital += Work.CapitalF(capital, changeIMB);
+
+            indexForCalc++;
+        }
+        return outProc;
     }
 
-
-    public static double Rc(double capital, double outProc) {
+    public static double rc(double capital, double outProc) {
         return capital * outProc / 100;
     }
 
-
-    public static double capitalInit(int indexForCalc) {
-        return Constants.MOEX_RATE[indexForCalc];
+    public static double capitalMinusCost(double capital, double cost) {
+        return capital - cost;
     }
 
+    public static double inflationCurrentYear(int indexForCalc) {
+        return Constants.INFLATION_RATE[indexForCalc];
+    }
+
+    public static double recalculcWithdrawalAmount(double withdrawalAmount, double inflation) {
+        return inflation / 100 * withdrawalAmount;
+    }
 
     public static double IndexChange(int imbYear) {
         return (Constants.MOEX_RATE[imbYear + 1] - Constants.MOEX_RATE[imbYear]) * 100 /
                 Constants.MOEX_RATE[imbYear];
     }
 
-
-    public static double recalculcWithdrawalAmount(double withdrawalAmount, double inflation) {
-        return inflation / 100 * withdrawalAmount;
+    public static double CapitalF (double capital, double changeIMB) {
+        return capital * changeIMB / 100;
     }
 
-
-    public static double inflationCurrentYear(int indexForCalc) {
-        return Constants.INFLATION_RATE[indexForCalc];
-    }
-
-
-    public static void Pr(double outProc, double GROWTHPERCENTAGE) {
-        outProc -= GROWTHPERCENTAGE;
-        System.out.println(outProc);
-    }
-
-
-    public static int EnterYear() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
-    }
 }
